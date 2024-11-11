9f5c4a40a1f341dd0995911b9efb87fa16bda5d0
seemab28 <naavadagiseema@gmail.com>
2024-11-11 15:22:30 +0000

    using return function

---
+++ Day-04/new_final_ret.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    add=a+b
+    return add
+
+def sub(a,b):
+    sub=a-b
+    return sub
+
+def mul(a,b):
+    mul=a*b
+    return mul
+
+print(add(10,5))
+print(sub(10,5))
+print(mul(10,5))
